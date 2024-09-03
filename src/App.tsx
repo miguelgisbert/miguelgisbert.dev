238254eef47f35986aa84091d0f56d0bf7950694
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2024-09-03 19:34:41 +0200

    React Refactor

---
+++ src/App.tsx
@@ -0,0 +1,317 @@
+import React from "react";
+import { Box, Typography, IconButton } from "@mui/material";
+import { LinkedIn, GitHub } from "@mui/icons-material";
+import ContactForm from "./components/ContactForm";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Box id="main" className="section wf-section">
+        <Box className="container w-container">
+          <Box className="div-block nameblock">
+            <img
+              src="/src/assets/images/profile.png"
+              loading="lazy"
+              sizes="(max-width: 479px) 48vw, 218px"
+              alt="Miguel Gisbert profile picture"
+              className="image-15"
+            />
+            <Box className="div-block-3">
+              <Typography variant="h1" className="heading mainname">
+                Miguel Gisbert Osuna
+              </Typography>
+              <Typography variant="h1" className="heading positionname">
+                Fullstack Web Developer
+              </Typography>
+            </Box>
+            <Typography variant="h1" className="heading initialsname">
+              mgo
+            </Typography>
+            <IconButton
+              href="https://www.linkedin.com/in/miguel-gisbert-osuna/"
+              target="_blank"
+              className="link-block-16 w-inline-block"
+              aria-label="LinkedIn"
+            >
+              <LinkedIn fontSize="inherit" />
+            </IconButton>
+            <IconButton
+              href="https://github.com/miguelgisbert/"
+              target="_blank"
+              className="link-block-17 w-inline-block"
+              aria-label="GitHub"
+            >
+              <GitHub fontSize="inherit" />
+            </IconButton>
+            <IconButton
+              href="https://www.npmjs.com/~miguelgisbert"
+              target="_blank"
+              className="link-block-18 w-inline-block"
+              aria-label="NPM"
+            >
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+      <Box id="expertise" className="expertise wf-section">
+        <Box className="expertise-container">
+          <Typography variant="h1" className="heading-2">
+            Expertise
+          </Typography>
+          <Box className="expertise-logos-block">
+            <a
+              title="Python"
+              href="https://python.org/"
+              target="_blank"
+              className="link-block-4 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/python.png"
+                loading="lazy"
+                alt="Python logo"
+                className="image-13"
+              />
+            </a>
+            <a
+              title="Flutter"
+              href="https://flutter.dev/"
+              target="_blank"
+              className="link-block-5 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/flutter.png"
+                loading="lazy"
+                alt="Flutter logo"
+                className="image-13"
+              />
+            </a>
+            <a
+              title="React"
+              href="https://react.dev/"
+              target="_blank"
+              className="link-block-8 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/react.svg"
+                loading="lazy"
+                alt="React logo"
+                className="image-14"
+              />
+            </a>
+            <a
+              title="Javascript"
+              href="https://en.wikipedia.org/wiki/JavaScript"
+              target="_blank"
+              className="link-block-8 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/Javascript.svg"
+                loading="lazy"
+                alt="Javascript logo"
+                className="image-8"
+              />
+            </a>
+            <a
+              title="html"
+              href="https://en.wikipedia.org/wiki/HTML"
+              target="_blank"
+              className="link-block-6 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/HTML5.svg"
+                loading="lazy"
+                alt="Html logo"
+                className="image-11"
+              />
+            </a>
+            <a
+              title="css"
+              href="https://en.wikipedia.org/wiki/CSS"
+              target="_blank"
+              className="link-block-7 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/CSS3.svg"
+                loading="lazy"
+                alt="Css logo"
+                className="image-7"
+              />
+            </a>
+            <a
+              title="php"
+              href="https://php.net"
+              target="_blank"
+              className="link-block-9 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/PHP-logo.svg"
+                loading="lazy"
+                alt="Php logo"
+                className="image-4"
+              />
+            </a>
+            <a
+              title="MySql"
+              href="https://www.mysql.com/"
+              target="_blank"
+              className="link-block-10 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/mysql.svg"
+                loading="lazy"
+                alt="MySql logo"
+                className="image-5"
+              />
+            </a>
+          </Box>
+        </Box>
+      </Box>
+      <Box id="portfolio" className="section-2 portfolio portfolio wf-section">
+        <Box className="container-2 w-container">
+          <Typography variant="h1" className="heading-2">
+            Portfolio
+          </Typography>
+          <Box className="portfolioblock">
+            <a
+              href="https://wearebluecode.com/rapid-prototyping/"
+              target="_blank"
+              className="link-block-11 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/RapidProto.svg"
+                loading="lazy"
+                width="200"
+                alt="Rapid prototyping preview"
+                className="image-2 div-block rapidprotoimage"
+              />
+            </a>
+            <a
+              href="https://wearebluecode.com/"
+              target="_blank"
+              className="link-block-12 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/Screenshot-from-2022-12-10-18-34-15.png"
+                loading="lazy"
+                width="200"
+                sizes="(max-width: 479px) 280px, 300px"
+                alt="wearebluecode preview"
+                className="image-2 div-block bcpicture"
+              />
+            </a>
+            <a
+              href="https://wearebluecode.com/ventures/"
+              target="_blank"
+              className="link-block-13 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/Screenshot-from-2022-12-10-18-37-39.png"
+                loading="lazy"
+                width="200"
+                sizes="(max-width: 479px) 280px, 300px"
+                alt="Ventures development preview"
+                className="venturespicture"
+              />
+            </a>
+            <a
+              href="https://wearebluecode.com/talent/ITRecruitment/"
+              target="_blank"
+              className="link-block-14 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/Screenshot-from-2022-12-10-18-43-06.png"
+                loading="lazy"
+                width="200"
+                alt="IT talent recruitment preview"
+                className="talentpicture"
+              />
+            </a>
+            <a
+              href="https://hireplek.com/"
+              target="_blank"
+              className="link-block-15 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/hireplek.png"
+                loading="lazy"
+                width="200"
+                alt="Hireplek"
+                className="royaltypicture"
+              />
+            </a>
+            <a
+              href="https://hrvendurance.com/"
+              target="_blank"
+              className="link-block-15 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/hrvendurance.png"
+                loading="lazy"
+                width="200"
+                alt="HRV Endurance"
+                className="royaltypicture"
+              />
+            </a>
+          </Box>
+        </Box>
+      </Box>
+      <Box id="education" className="education wf-section">
+        <Box className="container-2 educationcontainer w-container">
+          <Typography variant="h1" className="heading-2">
+            Education
+          </Typography>
+          <Box className="webflowblock">
+            <a
+              href="https://www.ua.es/en/"
+              target="_blank"
+              className="uadiv w-inline-block"
+            >
+              <img
+                src="/src/assets/images/logo-ua.jpg"
+                loading="lazy"
+                alt="University of Alicante logo"
+                className="image-20"
+              />
+            </a>
+          </Box>
+          <Box className="educationblock">
+            <a
+              href="https://eps.ua.es/en/"
+              target="_blank"
+              className="link-block-2 w-inline-block"
+            >
+              <img
+                src="/src/assets/images/cabecera.jpg"
+                loading="lazy"
+                alt="Polytechnic School logo"
+                className="image-18"
+              />
+              <Typography variant="h2" className="heading-3">
+                Computer Science Engineer
+              </Typography>
+            </a>
+            <a
+              href="https://educacio.ua.es/en/estudios-de-la-facultad-de-educacion/grado-cafd/grado-en-ciencias-de-la-actividad-fisica-y-el-deporte.html"
+              target="_blank"
+              className="link-block w-inline-block"
+            >
+              <img
+                src="/src/assets/images/55fc0e333859ae0799cb2dcb709aaf59_400x400.jpeg"
+                loading="lazy"
+                alt="Sport Science logo"
+                className="image-17"
+              />
+              <Typography variant="h2" className="heading-4">
+                Sport Science
+              </Typography>
+            </a>
+          </Box>
+        </Box>
+      </Box>
+      <ContactForm />
+    </>
+  );
+}
+
+export default App
